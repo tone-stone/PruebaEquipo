259efc6555a37a0ec00fdf3821376b1664f8aa5a
Tone <soportetecnicotone@gmail.com>
2020-10-06 13:13:06 -0700

    Primer Commit

---
+++ src/hola/Hola.java
@@ -0,0 +1,13 @@
+
+package hola;
+
+
+public class Hola {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
